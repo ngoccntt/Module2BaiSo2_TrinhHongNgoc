88cf5403512bf40fd0cc599a366fc04f16c9e8b7
ADMIN <ADMIN@LAB1-MAY06>
2023-09-18 15:21:37 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Module2BaiSo2_TrinhHongNgoc
+{
+    partial class frmBaiTap1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYourName = new System.Windows.Forms.TextBox();
+            this.txtYearOfBirth = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Year sinh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên của you";
+            // 
+            // txtYourName
+            // 
+            this.txtYourName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYourName.Location = new System.Drawing.Point(198, 74);
+            this.txtYourName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtYourName.Name = "txtYourName";
+            this.txtYourName.Size = new System.Drawing.Size(236, 26);
+            this.txtYourName.TabIndex = 1;
+            this.txtYourName.Leave += new System.EventHandler(this.txtYourName_Leave);
+            // 
+            // txtYearOfBirth
+            // 
+            this.txtYearOfBirth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYearOfBirth.Location = new System.Drawing.Point(198, 119);
+            this.txtYearOfBirth.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtYearOfBirth.Name = "txtYearOfBirth";
+            this.txtYearOfBirth.Size = new System.Drawing.Size(236, 26);
+            this.txtYearOfBirth.TabIndex = 3;
+            this.txtYearOfBirth.TextChanged += new System.EventHandler(this.txtYearOfBirth_TextChanged);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(88, 253);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(86, 28);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "&Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(218, 253);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(86, 28);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(349, 253);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(86, 28);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmBaiTap1
+            // 
+            this.AcceptButton = this.btnShow;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(556, 393);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtYearOfBirth);
+            this.Controls.Add(this.txtYourName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmBaiTap1";
+            this.Text = "My name Project";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBaiTap1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYourName;
+        private System.Windows.Forms.TextBox txtYearOfBirth;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module2BaiSo2_TrinhHongNgoc
+{
+    public partial class frmBaiTap1 : Form
+    {
+        public frmBaiTap1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBaiTap1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r;
+            r = MessageBox.Show("Do you really want to close?", "Ex1",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question,
+                                MessageBoxDefaultButton.Button1);
+            if (r == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void txtYourName_Leave(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "You must enter Your Name");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void txtYearOfBirth_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Length > 0 && !Char.IsDigit(ctr.Text[ctr.Text.Length - 1]))
+                this.errorProvider1.SetError(ctr, "This is not avalid number");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            int age;
+            string s;
+            s = "My name is: " + txtYourName.Text + "\n";
+            age = DateTime.Now.Year - Convert.ToInt32(txtYearOfBirth.Text);
+            s = s + "Age: " + age.ToString();
+            MessageBox.Show(s);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtYourName.Clear();
+            txtYearOfBirth.Clear();
+            txtYourName.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
